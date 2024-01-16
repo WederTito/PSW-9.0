d066e5d14053ca2659c3434723e8376a2e3d27f6
wedertito <wedertito8@gmail.com>
2024-01-16 18:04:42 -0300

    Login

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -29,8 +30,25 @@ def cadastro(request):
                 username=username,
                 password=senha
             )
-            return redirect('/usuarios/login')
+            return redirect('/usuarios/logar')
         except:
             messages.add_message(request, constants.ERROR, 'Erro interno do Servidor')
             return redirect('/usuarios/cadastro')
 
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+        
+        if user:
+            auth.login(request, user)
+            messages.add_message(request, constants.SUCCESS, 'Logado!')
+            return redirect('/flashcard/novo_flashcard')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuário ou senha inválida.')
+            return redirect('/usuarios/logar/')
